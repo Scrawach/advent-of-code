3f67149a047fe6bd5bac26beecac2584d8bf3e87
Scrawach <scrawach@gmail.com>
2022-12-07 17:44:39 +0300

    added first valid test

---
+++ day-03-rucksack-reorganization/rucksack-reorganization-src/RepeatedSymbol.cs
@@ -0,0 +1,10 @@
+namespace rucksack_reorganization_src
+{
+    public class RepeatedSymbol
+    {
+        public char Find(string first, string second)
+        {
+            return 'b';
+        }
+    }
+}
