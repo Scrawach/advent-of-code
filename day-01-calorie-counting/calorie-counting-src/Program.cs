dad092cb176e85c291756b45b74da992889f8ecd
Scrawach <scrawach@gmail.com>
2022-12-07 10:25:25 +0300

    solved puzzle

---
+++ day-01-calorie-counting/calorie-counting-src/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+
+namespace day_01_calorie_counting
+{
+    public static class Program
+    {
+        private static readonly string WorkingDirectory = 
+            Environment.CurrentDirectory[..Environment.CurrentDirectory.IndexOf("bin")];
+        
+        private static void Main(string[] args)
+        {
+            var path = Path.Combine(WorkingDirectory, "input.txt");
+            
+            var mostCalories = new MostCalories(new InventoryDatabase(new Text(path)));
+            Console.WriteLine($"First Task Result: {mostCalories.Solve()}."); // First Task Result: 68923
+
+            var mostCaloriesSum = new MostCalories(3, new InventoryDatabase(new Text(path)));
+            Console.WriteLine($"Second Task Result: {mostCaloriesSum.Solve()}"); // First Task Result: 200044
+        }
+    }
+}
