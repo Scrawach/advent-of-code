ee05d7fa20a920fabf3fce8fc967e1657eac504d
Scrawach <scrawach@gmail.com>
2022-12-08 11:20:23 +0300

    added red test for priority

---
+++ day-03-rucksack-reorganization/rucksack-reorganization-src/Priority.cs
@@ -0,0 +1,10 @@
+namespace rucksack_reorganization_src
+{
+    public class Priority
+    {
+        public int Convert(char symbol)
+        {
+            return 0;
+        }
+    }
+}
---
+++ day-03-rucksack-reorganization/rucksack-reorganization-tests/PriorityTests.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using FluentAssertions;
+using NUnit.Framework;
+using rucksack_reorganization_src;
+
+namespace rucksack_reorganization_tests
+{
+    public class PriorityTests
+    {
+        [TestCaseSource(typeof(PriorityDataSource))]
+        public void WhenConvertSymbol_ThenShouldReturnItPriority(char input, int expected)
+        {
+            // arrange
+            var priority = new Priority();
+
+            // act
+            var code = priority.Convert(input);
+
+            // answer
+            code.Should().Be(expected);
+        }
+        
+        private class PriorityDataSource : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                var priority = 1;
+                
+                for (var i = 'a'; i <= 'z'; i++, priority++)
+                    yield return new object[] {i, priority};
+
+                for (var i = 'A'; i <= 'Z'; i++, priority++)
+                    yield return new object[] {i, priority};
+            }
+        }
+    }
+}
